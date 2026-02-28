af79d3b363a146d3f3d4264d97c21f2d766daad9
Daray Padilla <jorge.padilla5847@gmail.com>
2026-02-28 16:30:09 -0600

    1.5.2 MOdo oscuro amoled

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* sw.js - FIX: no devolver index.html para scripts (evitar MIME error) */
-const CACHE = "mi-cache-v13";
+const CACHE = "mi-cache-v14";
 const RUNTIME = "runtime-cache-v1";
 const MAX_RUNTIME_ENTRIES = 200;
 
